e4fa5695dc69f0b6c9adf6d3fdd873419f704658
Andreas Bentele <andreas.bentele@me.com>
2025-10-12 11:40:32 +0200

    Revert "exclude password from response with interceptor"
    This reverts commit b1d1dd6a2c8fd9488929289e684bb1225ce53972.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,8 +8,6 @@ import {
   Patch,
   Post,
   Query,
-  ClassSerializerInterceptor,
-  UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/CreateUser.dto';
@@ -25,7 +23,6 @@ export class UsersController {
     this.usersService.createUser(data);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
   @Get(':id')
   async findUser(@Param('id') id: number) {
     const user = await this.usersService.findOne(id);
